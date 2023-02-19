997b9f4b83c55e060cd52c1812b11f3555bef6ad
Mahin Hasan <mahinmtrs@gmail.com>
2023-02-19 12:52:38 +0600

    cleared unnecessary omment and function

--- js/app.js
+++ js/app.js
@@ -17,21 +17,11 @@ let sign = '';
 let resultValue = 0;
 let lastResultValue = '';
 let valueHasDot = false;
-let firstValueHaveDot = false;
-let secondValueHaveDot = false;
-
-// let startsWithZero= false;
 
 // adding click event listeners to all the number button
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', (element) => {
         let num = element.target.getAttribute('value')
-        console.log(num);
-        // console.log({
-        //     num,
-        //     isFirstValue,
-        //     isSecondValue
-        // });
         if (isFirstValue === false || (lastResultValue != "" && isFirstValue == true)) {
 
             if (lastResultValue != "" && isFirstValue === false) {
@@ -52,8 +42,6 @@ for (let i = 0; i < numbers.length; i++) {
 
 function getInput(input, value) {
     // now allowing multiple.
-    // const valueHasDot = value.includes(".");
-
     if (input === '.' && !valueHasDot) {
         valueHasDot = true;
     } else if (input === '.' && valueHasDot) {
@@ -80,145 +68,7 @@ function getInput(input, value) {
     return value;
 }
 
-function getNumbers(value) {
-    if (lastResultValue != "" && isFirstValue === false) {
-        lastResultValue = "";
-        // clearing last value when first value is taken again
-    }
-    // now allowing multiple .
-    if (value === '.' && !firstValueHaveDot) {
-        firstValueHaveDot = true;
-    } else if (value === '.' && firstValueHaveDot) {
-        return;
-    }
-    // validation multiple zero
-    if (value === '0' && firstValue == "0") {
-        return;
-    }
-    if (value !== '0' && firstValue == "0") {
-        firstValue = "";
-    }
 
-
-    if (secondValue === '' && sign === '') {
-        firstValue += value;
-        //new code for adding 0 when . is clicked
-        if (firstValue.startsWith('.')) {
-            let addZero = '0';
-            let numWithZero = addZero + firstValue;
-            console.log(numWithZero);
-            firstValue = numWithZero;
-        }
-        console.log({ t: typeof (firstValue), firstValue, value });
-        result.value = firstValue;
-        resultId.value = firstValue;
-        console.log('first value', firstValue);
-    }
-    // for 2nd value
-    if (firstValue != '' && sign != '') {
-        if (secondValue != '') {
-            resultId.value = '';
-            // clearing if second value exist
-        }
-        // validation multiple zero
-        if (value === '0' && secondValue == "0") {
-            return;
-        }
-        if (value !== '0' && secondValue == "0") {
-            firstValue = "";
-        }
-        secondValue += value;
-        //new code for adding 0 when . is clicked
-        if (secondValue.startsWith('.')) {
-            let addZeroSecond = '0';
-            let numWithZeroSecond = addZeroSecond + secondValue;
-            console.log(numWithZeroSecond);
-            secondValue = numWithZeroSecond;
-        }
-        result.value = secondValue;
-        // secondValue = +secondValue;
-        resultId.value = secondValue;
-        console.log('second value', secondValue);
-    }
-}
-
-function getFirstValue(firstV) {
-    if (lastResultValue != "" && isFirstValue === false) {
-        lastResultValue = "";
-        // clearing last value when first value is taken again
-    }
-    // now allowing multiple .
-    if (firstV === '.' && !firstValueHaveDot) {
-        firstValueHaveDot = true;
-    } else if (firstV === '.' && firstValueHaveDot) {
-        return;
-    }
-    // validation multiple zero
-    if (firstV === '0' && firstValue == "0") {
-        return;
-    }
-    if (firstV !== '0' && firstValue == "0") {
-        firstValue = "";
-    }
-
-
-    firstValue += firstV;
-
-    // if (firstValue.startsWith('0')) {
-    //     return;
-    // }
-    //new code for adding 0 when . is clicked
-    if (firstValue.startsWith('.')) {
-        let addZero = '0';
-        let numWithZero = addZero + firstValue;
-        console.log(numWithZero);
-        firstValue = numWithZero;
-    }
-
-    console.log({ t: typeof (firstValue), firstValue, firstV });
-
-    result.value = firstValue;
-    resultId.value = firstValue;
-    console.log('first value', firstValue)
-
-}
-
-function getSecondValue(secondV) {
-    //validating multiple .
-    if (secondV === '.' && !secondValueHaveDot) {
-        secondValueHaveDot = true;
-    } else if (secondV === '.' && secondValueHaveDot) {
-        return;
-    }
-
-    if (firstValue != '' && sign != '') {
-        if (secondValue != '') {
-            resultId.value = '';
-            // clearing if second value exist
-        }
-        // validation multiple zero
-        if (secondV === '0' && secondValue == "0") {
-            return;
-        }
-
-        if (secondV !== '0' && secondValue == "0") {
-            firstValue = "";
-        }
-
-        secondValue += secondV;
-        //new code for adding 0 when . is clicked
-        if (secondValue.startsWith('.')) {
-            let addZeroSecond = '0';
-            let numWithZeroSecond = addZeroSecond + secondValue;
-            console.log(numWithZeroSecond);
-            secondValue = numWithZeroSecond;
-        }
-        result.value = secondValue;
-        // secondValue = +secondValue;
-        resultId.value = secondValue;
-        console.log('second value', secondValue);
-    }
-}
 function getSign() {
     for (let i = 0; i < signs.length; i++) {
         signs[i].addEventListener('click', (element) => {
@@ -234,8 +84,6 @@ getSign();
 equals.addEventListener('click', () => {
     valueHasDot = false; // for dot validation
 
-    firstValueHaveDot = false; // for dot validation
-    secondValueHaveDot = false; // for dot validation
     // converting 
     firstValue = +firstValue;
     secondValue = +secondValue;
@@ -270,9 +118,6 @@ equals.addEventListener('click', () => {
     isSecondValue = false;
     sign = '';
     valueHasDot = false;
-    firstValueHaveDot = false;
-    secondValueHaveDot = false;
-
 })
 
 // AC Button.
@@ -287,98 +132,6 @@ clear.addEventListener('click', () => {
     sign = '';
     resultValue = 0;
     valueHasDot = false;
-    firstValueHaveDot = false;
-    secondValueHaveDot = false;
 })
 
 
-
-/*
-all comment get first value
- if(firstV==='.'){
-        firstV= firstV +'.';
-        firstV = +firstV;
-        console.log(typeof(firstV));
-    }
-    console.log(typeof (firstV), firstV);
-    let changeFirstv= parseFloat(firstV);
-    console.log(typeof(changeFirstv));
-
-    ..
-     if (firstV === '.') {
-        console.log(firstV);
-
-    }
-    ..
-    // resultId.value = firstValue;
-
-    // let noWithDecimal= parseFloat(firstValue);
-    // console.log('working?', noWithDecimal)
-
-    ..click btn
-        else if (sign === '.') {
-        firstValue = firstValue +'.'
-        console.log(firstValue);
-        // resultValue = firstValue / secondValue;
-    }
-//
- // new code
-    // let testVariable = firstValue;
-    // let oneDecimal;
-    // if (testVariable.includes('.')){
-    //     console.log('inside if')
-    //     oneDecimal +=   
-    // }
-
-    // new code ends
-    // firstValue = +firstValue; // converting from string to number
-
-    // end rec
-
-    // starts
-    // let last_input = input.slice(-1);
-    // if (firstV ==='.' && last_input==='.'){
-    //     return false;
-    // }
-
-
-    // ends
-
-
-
-    // new code
-    // let storeNumber = [];
-    // storeNumber.push(firstV);
-    // let stringNumber = firstV.toString();
-    // console.log(storeNumber);
-    // let stringNumber =firstV.toString();
-    // let integerDigits = parseFloat(stringNumber.split('.'), [0]);
-    // let decimalDigits = stringNumber.split('.')[1];
-    // // let newDecimalNum = integerDigits + '.' + decimalDigits;
-    // // console.log(newDecimalNum);
-    // let integerDisplay
-    // console.log(stringNumber,integerDigits,decimalDigits,integerDisplay)
-
-    // if (isNaN(integerDigits)) {
-    //     integerDisplay = ''
-    // } else {
-    //     integerDisplay = integerDigits.toLocaleString('en', {
-    //     maximumFractionDigits: 0})
-    // }
-    // if (decimalDigits != null) {
-    //     return `${integerDisplay}.${decimalDigits}`;
-    // } else {
-    //     return integerDisplay;
-    // }
-
-    // new code ends
-
-
-    /*
-    // if (firstV === '.') {
-    //     let addZero = '0';
-    //     addZero += firstV;
-    //     console.log(addZero);
-    //     //   firstValue += firstV;
-    // } 
-    */
